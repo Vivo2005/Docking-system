a4f5ff77185d93d6cedfd78efcdf529e0e8ce2b0
Vivo2005 <lvm1@kent.ac.uk>
2026-01-22 17:46:07 +0000

    part 1

--- docking_system.py
+++ docking_system.py
@@ -1,1 +1,8 @@
 
+ship_name= (input("Ship Name?")).replace(" ","_").upper()
+cargo_value= value(input("Estimated cargo value?"))
+
+def value(a):
+    A=(a).strip('£$')
+    return (float(A))
+print(cargo_value)
